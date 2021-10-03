abbfa56991a5652291ca4cb2f09bd896834e4d7c
Trevor Sawler <trevor.sawler@gmail.com>
2021-10-03 17:36:02 -0300

    update for clarity

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"embed"
+	"fmt"
 	"github.com/gobuffalo/pop"
 	"log"
 )
 
+const migrations = "./migrations"
 
 //go:embed templates
 var templateFS embed.FS
@@ -17,28 +19,35 @@ func main() {
 	}
 	defer tx.Close()
 
+	// generate a fizz up/down migration for users table
 	usersUp, _ := templateFS.ReadFile("templates/users.postgres.up.fizz")
 	usersDown, _ := templateFS.ReadFile("templates/users.postgres.down.fizz")
 
-	err = pop.MigrationCreate("./migrations", "users_table", "fizz", usersUp, usersDown)
+	err = pop.MigrationCreate(migrations, "users_table", "fizz", usersUp, usersDown)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// add auto update of updated_at function/trigger, and down migration
 	triggerUp, _ := templateFS.ReadFile("templates/users_trigger.postgres.up.sql")
 	triggerDown, _ := templateFS.ReadFile("templates/users_trigger.postgres.down.sql")
 
-	err = pop.MigrationCreate("./migrations", "users_trigger", "sql", triggerUp, triggerDown)
+	err = pop.MigrationCreate(migrations, "users_trigger", "sql", triggerUp, triggerDown)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// create a file migrator
 	fm, err := pop.NewFileMigrator("./migrations", tx)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// run the migrations
 	err = fm.Up()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Println("Done!")
 }
