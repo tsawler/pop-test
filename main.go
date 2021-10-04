8cc225a2afab99cd774d4f8f777c812dc3e52103
Trevor Sawler <trevor.sawler@gmail.com>
2021-10-04 12:24:42 -0300

    wait one second between creating migrations

--- main.go
+++ main.go
@@ -5,12 +5,17 @@ import (
 	"fmt"
 	"github.com/gobuffalo/pop"
 	"log"
+	"time"
 )
 
-const migrations = "./migrations"
+const migrationPath = "./migrations"
 
 //go:embed templates
 var templateFS embed.FS
+
+var migrations struct {
+	migrationMap map[string]pop.Migrations
+}
 
 func main() {
 	tx, err := pop.Connect("development")
@@ -22,8 +27,7 @@ func main() {
 	// generate a fizz up/down migration for users table
 	usersUp, _ := templateFS.ReadFile("templates/users.postgres.up.fizz")
 	usersDown, _ := templateFS.ReadFile("templates/users.postgres.down.fizz")
-
-	err = pop.MigrationCreate(migrations, "users_table", "fizz", usersUp, usersDown)
+	err = createMigration(usersUp, usersDown, "users_table", "fizz")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -31,8 +35,15 @@ func main() {
 	// add auto update of updated_at function/trigger, and down migration
 	triggerUp, _ := templateFS.ReadFile("templates/users_trigger.postgres.up.sql")
 	triggerDown, _ := templateFS.ReadFile("templates/users_trigger.postgres.down.sql")
+	err = createMigration(triggerUp, triggerDown, "users_trigger", "sql")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	err = pop.MigrationCreate(migrations, "users_trigger", "sql", triggerUp, triggerDown)
+	// test table
+	whateverUp, _ := templateFS.ReadFile("templates/whatever.postgres.up.fizz")
+	whateverDown, _ := templateFS.ReadFile("templates/whatever.postgres.down.fizz")
+	err = createMigration(whateverUp, whateverDown, "whatever", "fizz")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -50,4 +61,14 @@ func main() {
 	}
 
 	fmt.Println("Done!")
+}
+
+func createMigration(up, down []byte, migrationName, migrationType string) error {
+	err := pop.MigrationCreate(migrationPath, migrationName, migrationType, up, down)
+	if err != nil {
+		return err
+	}
+	// strangely, we have to wait one second so that the migration name is not duplicated
+	time.Sleep(1 * time.Second)
+	return nil
 }
